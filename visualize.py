2e124387444fb988e8692a24479bcf6fbd715fd9
530924@mail.muni.cz <marek.dytrt@seznam.cz>
2026-04-09 15:49:56 +0200

    added axis titles

--- visualize.py
+++ visualize.py
@@ -3,4 +3,9 @@ import seaborn as sns
 def plot(prices: list[int]) -> None:
     line_plot = sns.lineplot(prices)
     fig = line_plot.get_figure()
+
+    line_plot.set_xlabel("Rounds", fontsize=12)
+    line_plot.set_ylabel("Barrel price ($)", fontsize=12)
+    line_plot.set_title("Crude oil price", fontsize=15)
+
     fig.savefig("res.png")
